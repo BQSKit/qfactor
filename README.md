d1414440f8e833023b91e9898b70601a3fe3bbae
Ed Younis <edyounis123@gmail.com>
2020-10-21 11:47:28 -0700

    Packaging

---
+++ README.md
@@ -0,0 +1,20 @@
+# Circuit SVD Optimizer
+
+This optimizer can optimize the distance between a circuit, a sequence of
+unitary gates, and a target unitary matrix. This optimizer uses an analytic
+method based on the SVD operation.
+
+## Installation
+
+The best way to install this python package is with pip.
+
+```
+pip install csvdopt
+```
+
+## Usage
+
+The csvdopt package can be used to optimize a specified circuit.
+You can specify your circuit with a list of Gate objects and pass them to
+the optimize function. See [an example](https://github.com/edyounis/csvdopt/blob/master/examples/toffoli_synthesis.py).
+
---
+++ setup.py
@@ -0,0 +1,52 @@
+from os import path
+from setuptools import setup, find_namespace_packages
+
+
+# Use READEME for long description.
+root_dir = path.abspath( path.dirname( __file__ ) )
+readme_path = path.join( root_dir, "README.md" )
+
+with open( readme_path, encoding = "utf-8" ) as f:
+    long_description = f.read()
+
+# use requirements.txt for requirements.
+req_path = path.join( root_dir, "requirements.txt" )
+
+with open( req_path ) as f:
+    requirements = f.read().split( '\n' )
+    requirements.remove( '' )
+
+setup( name = "csvdopt",
+       version = "1.0.0",
+       description = "Circuit SVD Optimizer",
+       long_description = long_description,
+       long_description_content_type = "text/markdown",
+       url = "https://github.com/edyounis/csvdopt",
+       author = "Ed Younis",
+       author_email = "edyounis@lbl.gov",
+       classifiers = [
+           "Development Status :: 5 - Production/Stable",
+           "Environment :: Console",
+           "Intended Audience :: Developers",
+           "Intended Audience :: Science/Research",
+           "Operating System :: OS Independent",
+           "Programming Language :: Python :: 3 :: Only",
+           "Programming Language :: Python :: 3.5",
+           "Programming Language :: Python :: 3.6",
+           "Programming Language :: Python :: 3.7",
+           "Topic :: Scientific/Engineering",
+           "Topic :: Scientific/Engineering :: Mathematics",
+           "Topic :: Scientific/Engineering :: Physics",
+           "Topic :: Software Development :: Compilers"
+       ],
+       keywords = "quantum circuit optimization optimizer",
+       project_urls = {
+           "Bug Tracker": "https://github.com/edyounis/csvdopt/issues",
+           "Source Code": "https://github.com/edyounis/csvdopt"
+       },
+       packages = find_namespace_packages( exclude = [ "tests*",
+                                                       "examples*" ] ),
+       install_requires = requirements,
+       python_requires = ">=3.5, <4",
+)
+
